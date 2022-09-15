50ec6cd6bd1aa9c0fe5aa245080c7f211bdb7615
zouqiang <410755274@qq.com>
2022-09-15 10:00:38 +0800

    增加mysql支持

--- migrations_mysql/1_init_log.go
+++ migrations_mysql/1_init_log.go
@@ -16,17 +16,19 @@ func init() {
 				[[method]]    VARCHAR(100) DEFAULT "get" NOT NULL,
 				[[status]]    INTEGER DEFAULT 200 NOT NULL,
 				[[auth]]      VARCHAR(100) DEFAULT "guest" NOT NULL,
-				[[ip]]        VARCHAR(100) DEFAULT "127.0.0.1" NOT NULL,
+				[[remoteIp]]  VARCHAR(100) DEFAULT "127.0.0.1" NOT NULL,
+			    [[userIp]]    VARCHAR(100) DEFAULT "127.0.0.1" NOT NULL,
 				[[referer]]   VARCHAR(100) DEFAULT "" NOT NULL,
-				[[userAgent]] VARCHAR(100) DEFAULT "" NOT NULL,
+				[[userAgent]] VARCHAR(2048) DEFAULT "" NOT NULL,
 				[[meta]]      JSON NOT NULL,
 				[[created]]   VARCHAR(100) DEFAULT "" NOT NULL,
 				[[updated]]   VARCHAR(100) DEFAULT "" NOT NULL
 			);
 
-			-- CREATE INDEX _request_status_idx on {{_requests}} ([[status]]);
-			-- CREATE INDEX _request_auth_idx on {{_requests}} ([[auth]]);
-			-- CREATE INDEX _request_ip_idx on {{_requests}} ([[ip]]);
+			CREATE INDEX _request_status_idx on {{_requests}} ([[status]]);
+			CREATE INDEX _request_auth_idx on {{_requests}} ([[auth]]);
+			CREATE INDEX _request_remote_ip_idx on {{_requests}} ([[remoteIp]]);
+			CREATE INDEX _request_user_ip_idx on {{_requests}} ([[userIp]]);
 			-- TODO CREATE INDEX _request_created_hour_idx on {{_requests}} (DATA_FORMAT([[created]],'%Y-%m-%d %H:00:00'));
 		`).Execute()
 
